7d698070bef281ea52833ba2cdef7e056726848e
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-07 12:51:41 +0100

    ajout css

--- app.py
+++ app.py
@@ -3,9 +3,30 @@ from fastapi import FastAPI, Request # Outils pour API
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI() #Créer une instance 
 
+# Init request motor
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 # Créer une route 
+
+@app.get("/home", response_class=HTMLResponse)
+async def read_home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request" : request,
+         "name": "Mario",
+         "job": "Plombier",
+         "brother": "Luigi",
+         "friends" : ["Toad", "Peach", 
+                      "Donkey Kong", "Yoshi"]
+        }
+    )
+
 
 @app.get ("/")
 async def read_root():
