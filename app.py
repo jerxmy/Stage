8d7c9fcd78e6e1225c068f249b69c80c8dc7f48c
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-07 11:17:49 +0100

    ajout html + package

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-import uvicorn
+import uvicorn # équivalent (plus ou moins) d'un serveur web 
+from fastapi import FastAPI, Request # Outils pour API
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-from fastapi import FastAPI
+app = FastAPI() #Créer une instance 
 
-app = FastAPI()
+# Créer une route 
+
+@app.get ("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/test")
+async def read_test():
+    return {"message" : "Hello Test"}
+
+# Lancement du serveur
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
