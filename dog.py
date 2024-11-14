2f9b19f9f2928b2600699decd8d9f1be9f26b5ea
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-14 12:19:27 +0100

    update

--- dog.py
+++ dog.py
@@ -1,28 +1,12 @@
 class Dog:
 
-    def __init__(self, name, age=0):
+    def __init__(self, name,):
         # constructeur
         # self = eq. "this"
         self.name = name
         self.speak()
-        # encapsultation = attribut privé
-        self.__age = age
 
     def speak(self):
         # methods
         # qui connait l'instance (self)
         print(self.name, ": Woof !")
-        # print(self.__age) impossible
-    
-    def get_age(self):
-        # guetter / Accesseur
-        if self.__age > 50:
-            return "Nan..."
-        return self.__age
-
-    def set_age(self, age):
-        # Setter / Mutateur
-        if int(age) and age > 0:
-            self.__age = age
-        else :
-            print("ERREUR")
