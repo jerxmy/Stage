838a59e543e49e84d236264611ab4115ac99dbc6
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-14 11:26:58 +0100

    update

--- poo.py
+++ poo.py
@@ -14,19 +14,29 @@ class Cat:
         # qui connait l'instance (self)
         print(self.name, ": Miaou !")
         # print(self.__age) impossible
+    
+    def get_age(self):
+        # guetter / Accesseur
+        if self.__age > 50:
+            return "Nan..."
+        return self.__age
 
+    def set_age(self, age):
+        # Setter / Mutateur
+        if int(age) and age > 0:
+            self.__age = age
+        else :
+            print("ERREUR")
 
-my_cat = Cat(name="Grominet")
+my_cat = Cat(name="Grominet", age=79)
+print(my_cat.get_age())
 
 print(my_cat)
 print(my_cat.name, my_cat.legs)
 
 my_cat.speak()
 
-cat2 = Cat()
-cat2.name = "Garfield"
+cat2 = Cat(name="Garfield")
 cat2.legs -=1
 print(cat2.name, cat2.legs)
-
-cat2.speak()
 
