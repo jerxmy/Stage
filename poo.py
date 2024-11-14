3f2bdc8483f11489d5ea2e4abf2f6efbe21a9384
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-14 11:07:01 +0100

    update

--- poo.py
+++ poo.py
@@ -1,16 +1,32 @@
 class Cat:
-    def __init__(self):
+    legs = 4
+
+    def __init__(self, name, age=0):
         # constructeur
         # self = eq. "this"
-        self.name = "Grominet"
+        self.name = name
+        self.speak()
+        # encapsultation = attribut privé
+        self.__age = age
+
+    def speak(self):
+        # methods
+        # qui connait l'instance (self)
+        print(self.name, ": Miaou !")
+        # print(self.__age) impossible
 
 
-my_cat = Cat()
+my_cat = Cat(name="Grominet")
 
 print(my_cat)
-print(my_cat.name)
+print(my_cat.name, my_cat.legs)
+
+my_cat.speak()
 
 cat2 = Cat()
 cat2.name = "Garfield"
-print(cat2.name)
+cat2.legs -=1
+print(cat2.name, cat2.legs)
+
+cat2.speak()
 
