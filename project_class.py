4812c035092ba75e4c081812bd150d742967d3a8
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-18 09:27:34 +0100

    update

--- project_class.py
+++ project_class.py
@@ -10,3 +10,34 @@ class Project(SQLModel, table=True):
     image : str
     rendu : str 
     url : Optional[str] = None
+
+    @classmethod
+    def get_all(cls) -> list[dict]:
+        pass
+
+    @classmethod
+    def get_one(cls, id: int) -> Optional[dict]:
+        pass
+
+    @classmethod
+    def insert (cls,
+                rubrique : str,
+                nom : str,
+                image : str,
+                rendu : str,
+                ) -> dict :
+        pass
+    
+    @classmethod
+    def update (cls,
+                id: int,
+                rubrique : Optional[str] = None,
+                nom : Optional[str] = None,
+                image : Optional[str] = None,
+                rendu : Optional[str] = None,
+                ) -> Optional[dict] :
+        pass
+
+    @classmethod
+    def update (cls, id: int) -> bool:
+        pass
