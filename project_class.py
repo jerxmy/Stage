b33ec23de2041543c90175c2adcf7a20f6a288c5
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-18 10:30:24 +0100

    item

--- project_class.py
+++ project_class.py
@@ -1,43 +1,63 @@
+# projet_class.py
 from sqlmodel import SQLModel, Field
-from typing import Optional
+from typing import Optional, List
+from database import get_session
+from sqlmodel import select
 
-class Project(SQLModel, table=True):
-    # Definition de la structure de la table = une classe
+
+class Projet(SQLModel, table=True):
+    # Définition de la structure de la table = une classe
     __tablename__ = "Projects"
     id: Optional[int] = Field(default=None, primary_key=True)
-    rubrique : str
-    nom : str
-    image : str
-    rendu : str 
-    url : Optional[str] = None
+    rubrique: str
+    nom: str
+    image: str
+    rendu: str
+    url: Optional[str]
 
     @classmethod
-    def get_all(cls) -> list[dict]:
-        pass
+    def get_all(cls) -> List[dict]:
+        with get_session() as session:
+            statement = select(cls)
+            results = session.exec(statement).all()
+            return [  projet.model_dump() for projet in results  ]
 
     @classmethod
-    def get_one(cls, id: int) -> Optional[dict]:
-        pass
+    def get_one (cls, id: int) -> Optional[dict] :
+        with get_session() as session:
+            statement = select(cls).where(cls.id == id)
+            result = session.exec(statement).first()
+            return result.model_dump() if result else None
+        
 
     @classmethod
-    def insert (cls,
-                rubrique : str,
-                nom : str,
-                image : str,
-                rendu : str,
-                ) -> dict :
-        pass
-    
-    @classmethod
-    def update (cls,
-                id: int,
-                rubrique : Optional[str] = None,
-                nom : Optional[str] = None,
-                image : Optional[str] = None,
-                rendu : Optional[str] = None,
-                ) -> Optional[dict] :
-        pass
+    def insert (cls, rubrique: str, nom : str, image: str, rendu: str) -> dict:
+        with get_session() as session:
+            item = cls(rubrique=rubrique, nom=nom, image=image, rendu=rendu)
+            session.add(item)
+            session.commit()
+            session.refresh(item)
+
+
 
     @classmethod
     def update (cls, id: int) -> bool:
         pass
+
+
+
+if __name__ == '__main__':
+    projets = Projet.get_all()
+    print(projets)
+
+    projet = Projet.get_one(id=1)
+    print(projet)
+
+    projet = Projet.insert(
+        rubrique="retardataire",
+        nom="Quentin",
+        image="image.jpg",
+        rendu="2024-12-24"
+    )
+
+    
