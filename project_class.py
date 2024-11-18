ff935e632d6376ba689d9762808a9d0f5c530603
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-18 11:37:05 +0100

    gygg

--- project_class.py
+++ project_class.py
@@ -40,25 +40,61 @@ class Projet(SQLModel, table=True):
             return item.model_dump()
 
 
+    @classmethod
+    def delete (cls, id: int) -> bool:
+        with get_session() as session:
+            statement = select(cls).where(cls.id == id)
+            result = session.exec(statement).first()
+            if result:
+                session.delete(result)
+                session.commit()
+                return True
+            return False
+
 
     @classmethod
-    def update (cls, id: int) -> bool:
-        pass
-
-
+    def update (cls, 
+                id: int,
+                rubrique: Optional[str] = None,
+                nom : Optional[str] = None,
+                image : Optional[str] = None,
+                rendu : Optional[str] = None,
+                ) -> Optional[dict]:
+         with get_session() as session:
+            statement = select(cls).where(cls.id == id)
+            result = session.exec(statement).first()
+            if result : 
+                if rubrique is not None:
+                    result.rubrique = rubrique
+                if nom is not None:
+                    result.nom = nom
+                if image is not None:
+                    result.image = image
+                if rendu is not None:
+                    result.rendu = rendu
+                session.add(result)
+                session.commit()
+                session.refresh(result)
+                return result.model_dump()
+            return None
 
 if __name__ == '__main__':
     projets = Projet.get_all()
     print(projets)
 
+    projet = Projet.update(id=1, nom="Richnou")
+
     projet = Projet.get_one(id=1)
     print(projet)
 
-    projet = Projet.insert(
-        rubrique="retardataire",
-        nom="Quentin",
-        image="image.jpg",
-        rendu="2024-12-24"
-    )
+    # projet = Projet.insert(
+    #     rubrique="retardataire",
+    #     nom="Quentin",
+    #     image="image.jpg",
+    #     rendu="2024-12-24"
+    # )
+
+    ok = Projet.delete(id=6)
+    print(ok)
 
     
--- stage_class.py
+++ stage_class.py
@@ -1,6 +1,18 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, select
 from faker import Faker
-from typing import Optional
+from typing import Optional, List
+from database import get_session
+
+
+def fake_stage():
+    fake = Faker("fr_FR")
+    return Stage(
+        poste=fake.job(),
+        entreprise=fake.company(),
+        expedition=fake.date_this_month().isoformat(),
+        url=fake.url()
+    )
+
 
 class Stage(SQLModel, table=True):
     # Definition de la structure de la table = une classe
@@ -11,11 +23,85 @@ class Stage(SQLModel, table=True):
     expedition : str
     url : Optional[str] = None
 
-def fake_stage():
-    fake = Faker("fr_FR")
-    return Stage(
-        poste=fake.job(),
-        entreprise=fake.company(),
-        expedition=fake.date_this_month().isoformat(),
-        url=fake.url()
+
+    @classmethod
+    def get_all(cls) -> List[dict]:
+        with get_session() as session:
+            statement = select(cls)
+            results = session.exec(statement).all()
+            return [  stage.model_dump() for stage in results  ]
+
+    @classmethod
+    def get_one (cls, id: int) -> Optional[dict] :
+        with get_session() as session:
+            statement = select(cls).where(cls.id == id)
+            result = session.exec(statement).first()
+            return result.model_dump() if result else None
+        
+
+    @classmethod
+    def insert (cls, poste: str, entreprise : str, expedition: str, url: str) -> dict:
+        with get_session() as session:
+            item = cls(poste=poste, entreprise=entreprise, expedition=expedition, url=url)
+            session.add(item)
+            session.commit()
+            session.refresh(item)
+            return item.model_dump()
+
+
+    @classmethod
+    def delete (cls, id: int) -> bool:
+        with get_session() as session:
+            statement = select(cls).where(cls.id == id)
+            result = session.exec(statement).first()
+            if result:
+                session.delete(result)
+                session.commit()
+                return True
+            return False
+
+
+    @classmethod
+    def update (cls, 
+                id: int,
+                poste: Optional[str] = None,
+                entreprise : Optional[str] = None,
+                expedition : Optional[str] = None,
+                url : Optional[str] = None,
+                ) -> Optional[dict]:
+         with get_session() as session:
+            statement = select(cls).where(cls.id == id)
+            result = session.exec(statement).first()
+            if result : 
+                if poste is not None:
+                    result.poste = poste
+                if entreprise is not None:
+                    result.entreprise = entreprise
+                if expedition is not None:
+                    result.expedition = expedition
+                if url is not None:
+                    result.url = url
+                session.add(result)
+                session.commit()
+                session.refresh(result)
+                return result.model_dump()
+            return None
+
+if __name__ == '__main__':
+    stages = Stage.get_all()
+    print(stages)
+
+    stage = Stage.update(id=1000, entreprise="Richnou")
+
+    stage = Stage.get_one(id=1000)
+    print(stage)
+
+    stage = Stage.insert(
+        poste="",
+        entreprise="",
+        expedition="",
+        url=""
     )
+
+    ok = Stage.delete(id=6)
+    print(ok)
