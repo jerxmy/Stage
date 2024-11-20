e856c1ba546ea5166658290cd1c3315f9f7fc7de
jerxmy <jeremy.chambon@mail-esd.com>
2024-11-20 10:36:14 +0100

    add

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import uvicorn # équivalent (plus ou moins) d'un serveur web
 from fastapi import FastAPI, Request # Outils pour API
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
 from fastapi.staticfiles import StaticFiles
 
+from project_routes import router as projet_routes
+
 app = FastAPI() #Créer une instance 
+
+app.include_router(projet_routes)
 
 # Init request motor
 templates = Jinja2Templates(directory="templates")
@@ -38,4 +41,4 @@ async def read_test():
 
 # Lancement du serveur
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
---
+++ project_routes.py
@@ -0,0 +1,36 @@
+from fastapi import Request, APIRouter, HTTPException
+from project_model import Projet
+from typing import List, Optional
+
+router = APIRouter()
+
+@router.get("/projets", response_model=List[Projet])
+def get_all_projets():
+    results = Projet.get_all()
+    return results
+
+@router.get("/projets/{id}", response_model=Projet)
+def get_one_projet(id : int):
+    result = Projet.get_one(id=id)
+    if result is None:
+        raise HTTPException(status_code=404, detail="Not Found... Try again")
+    return result
+
+@router.post("/projets", response_model=Projet)
+async def create_projet(request: Request):
+    body = await request.json()
+    rubrique = body.get("rubrique")
+    nom = body.get("nom")
+    image = body.get("image", "default.jpg")
+    rendu = body.get("rendu")
+
+    if not rubrique or not nom or not rendu:
+        raise HTTPException(status_code=400, detail="Missing required fields")
+    
+    result = Projet.insert(
+        rubrique= rubrique,
+        nom= nom,
+        image= image,
+        rendu= rendu
+    )
+    return result
